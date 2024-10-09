3b6f62917e55577252e30b4617069a5457b98d93
Yaniv Daye <yaniv120892@gmail.com>
2024-10-09 12:58:01 +0300

    add ping endpoint

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,6 +4,9 @@ import categoryRouter from '@src/routers/categoryRouter';
 
 const router = express.Router();
 
+router.get('/ping', (req, res) => {
+  res.send('ok');
+});
 router.use('/transactions', transactionRouter);
 router.use('/categories', categoryRouter);
 
