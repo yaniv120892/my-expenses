b7c796c618399d9727aeda3c3c57d1c9dbfd8088
Yaniv Daye <yaniv120892@gmail.com>
2025-05-13 14:39:24 +0300

    fix expose scheduled-transactions endpoints

--- dist/routers/index.js
+++ dist/routers/index.js
@@ -7,10 +7,11 @@ const express_1 = __importDefault(require("express"));
 const transactionRouter_1 = __importDefault(require("../routers/transactionRouter"));
 const categoryRouter_1 = __importDefault(require("../routers/categoryRouter"));
 const webhookRouter_1 = __importDefault(require("../routers/webhookRouter"));
+const scheduledTransactionRouter_1 = __importDefault(require("routers/scheduledTransactionRouter"));
 const router = express_1.default.Router();
 router.use('/webhook', webhookRouter_1.default);
 router.use('/api/transactions', transactionRouter_1.default);
-router.use('/api/scheduled-transactions', transactionRouter_1.default);
+router.use('/api/scheduled-transactions', scheduledTransactionRouter_1.default);
 router.use('/api/categories', categoryRouter_1.default);
 router.get('/', (req, res) => {
     res.send('ok');
