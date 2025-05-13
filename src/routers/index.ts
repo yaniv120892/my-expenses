3bd0bc5d60688bea0ae1eb1b26d47d6be4150dae
Yaniv Daye <yaniv120892@gmail.com>
2025-05-13 14:59:03 +0300

    support trigger proccess  scheduled transactions

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,12 +3,24 @@ import transactionRouter from '../routers/transactionRouter';
 import categoryRouter from '../routers/categoryRouter';
 import webhookRouter from '../routers/webhookRouter';
 import scheduledTransactionRouter from '../routers/scheduledTransactionRouter';
+import scheduledTransactionService from '../services/scheduledTransactionService';
 
 const router = express.Router();
 router.use('/webhook', webhookRouter);
 router.use('/api/transactions', transactionRouter);
 router.use('/api/scheduled-transactions', scheduledTransactionRouter);
 router.use('/api/categories', categoryRouter);
+
+router.post('/api/process-scheduled-transactions', async (req, res, next) => {
+  try {
+    const today = new Date();
+    await scheduledTransactionService.processDueScheduledTransactions(today);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.get('/', (req, res) => {
   res.send('ok');
 });
