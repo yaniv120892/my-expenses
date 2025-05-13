62d5e473ad5b0da6765925b73f61417eed9ac51c
Yaniv Daye <yaniv120892@gmail.com>
2025-05-13 16:23:02 +0300

    fix scheduled transaction trigger

--- dist/routers/index.js
+++ dist/routers/index.js
@@ -8,22 +8,11 @@ const transactionRouter_1 = __importDefault(require("../routers/transactionRoute
 const categoryRouter_1 = __importDefault(require("../routers/categoryRouter"));
 const webhookRouter_1 = __importDefault(require("../routers/webhookRouter"));
 const scheduledTransactionRouter_1 = __importDefault(require("../routers/scheduledTransactionRouter"));
-const scheduledTransactionService_1 = __importDefault(require("../services/scheduledTransactionService"));
 const router = express_1.default.Router();
 router.use('/webhook', webhookRouter_1.default);
 router.use('/api/transactions', transactionRouter_1.default);
 router.use('/api/scheduled-transactions', scheduledTransactionRouter_1.default);
 router.use('/api/categories', categoryRouter_1.default);
-router.post('/api/process-scheduled-transactions', async (req, res, next) => {
-    try {
-        const today = new Date();
-        await scheduledTransactionService_1.default.processDueScheduledTransactions(today);
-        res.status(200).json({ success: true });
-    }
-    catch (error) {
-        next(error);
-    }
-});
 router.get('/', (req, res) => {
     res.send('ok');
 });
