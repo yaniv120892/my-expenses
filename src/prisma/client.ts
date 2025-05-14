62e625a2715a81fb5bb33e88d5213a4f73119048
Yaniv Daye <yaniv120892@gmail.com>
2025-05-14 20:28:09 +0300

    add prisma data proxy

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prisma = new PrismaClient({
   log: ['warn', 'error'],
-});
+}).$extends(withAccelerate());
 
 export default prisma;
