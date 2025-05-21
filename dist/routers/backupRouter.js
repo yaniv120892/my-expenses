d0b3e54b5938c81fa149b83f91d92bb7433b003f
Yaniv Daye <yaniv120892@gmail.com>
2025-05-21 23:20:26 +0300

    add TODO

--- dist/routers/backupRouter.js
+++ dist/routers/backupRouter.js
@@ -7,5 +7,6 @@ const express_1 = __importDefault(require("express"));
 const handleRequest_1 = require("../utils/handleRequest");
 const backupController_1 = __importDefault(require("../controllers/backupController"));
 const router = express_1.default.Router();
+//TODO: trigger this endpoint from a cron job
 router.get('/transactions', (0, handleRequest_1.handleRequest)(() => backupController_1.default.backupTransactions(), 200));
 exports.default = router;
