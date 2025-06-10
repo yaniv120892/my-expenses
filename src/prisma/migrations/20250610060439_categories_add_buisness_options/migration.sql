3e8163790216def4c11e6294b83fa50207f2ca41
Yaniv Daye <yaniv120892@gmail.com>
2025-06-10 23:35:33 +0300

    add business categories

---
+++ src/prisma/migrations/20250610060439_categories_add_buisness_options/migration.sql
@@ -0,0 +1,12 @@
+-- Insert parent categories
+INSERT INTO "Category" (id, name, "parentId") VALUES
+  ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business', NULL);
+
+
+-- Insert children categories
+INSERT INTO "Category" (id, name, "parentId") VALUES
+  ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Incomes', '66e6ec1e-083e-4305-b933-039db6529f9b'),
+  ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Equipment', '66e6ec1e-083e-4305-b933-039db6529f9b'),
+  ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Other expenses', '66e6ec1e-083e-4305-b933-039db6529f9b'),
+  
+
