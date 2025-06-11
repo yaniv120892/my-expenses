6d714395ee338024f29dafb9ee2c3ca2369afaca
Yaniv Daye <yaniv120892@gmail.com>
2025-06-11 09:43:44 +0300

    impl

--- src/prisma/migrations/20250610060439_categories_add_buisness_options/migration.sql
+++ src/prisma/migrations/20250610060439_categories_add_buisness_options/migration.sql
@@ -7,6 +7,6 @@ INSERT INTO "Category" (id, name, "parentId") VALUES
 INSERT INTO "Category" (id, name, "parentId") VALUES
   ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Incomes', '66e6ec1e-083e-4305-b933-039db6529f9b'),
   ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Equipment', '66e6ec1e-083e-4305-b933-039db6529f9b'),
-  ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Other expenses', '66e6ec1e-083e-4305-b933-039db6529f9b'),
+  ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Other expenses', '66e6ec1e-083e-4305-b933-039db6529f9b');
   
 
