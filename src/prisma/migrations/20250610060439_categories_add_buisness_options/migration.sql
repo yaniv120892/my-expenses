23f930bab586fa07e4c3cfedfa167f8c7421788a
Yaniv Daye <yaniv120892@gmail.com>
2025-06-11 09:45:31 +0300

    category sql - fix unique uuid

--- src/prisma/migrations/20250610060439_categories_add_buisness_options/migration.sql
+++ src/prisma/migrations/20250610060439_categories_add_buisness_options/migration.sql
@@ -5,8 +5,8 @@ INSERT INTO "Category" (id, name, "parentId") VALUES
 
 -- Insert children categories
 INSERT INTO "Category" (id, name, "parentId") VALUES
-  ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Incomes', '66e6ec1e-083e-4305-b933-039db6529f9b'),
-  ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Equipment', '66e6ec1e-083e-4305-b933-039db6529f9b'),
-  ('66e6ec1e-083e-4305-b933-039db6529f9b', 'Business - Other expenses', '66e6ec1e-083e-4305-b933-039db6529f9b');
+  ('0369180d-981f-49a2-80f8-89387c2b49a1', 'Business - Incomes', '66e6ec1e-083e-4305-b933-039db6529f9b'),
+  ('48b3d0df-68ba-41d0-91b6-6b33e87a410c', 'Business - Equipment', '66e6ec1e-083e-4305-b933-039db6529f9b'),
+  ('6fd49628-b2db-43bb-98a3-1051be42de19', 'Business - Other expenses', '66e6ec1e-083e-4305-b933-039db6529f9b');
   
 
